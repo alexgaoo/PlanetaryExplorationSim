844a64897a56ddfbf39c5b6239ca61f674c9a3ff
Alex Gao <alexgao12@gmail.com>
2016-06-13 14:13:57 -0700

    Added rover model and textures

---
+++ rover.h
@@ -0,0 +1,21 @@
+// This is a .h file for the model: rover
+
+// Positions: 9707
+// Texels: 21549
+// Normals: 9707
+// Faces: 10286
+// Vertices: 30858
+// Materials: 10
+
+const int roverVertices;
+const float roverPositions[92574];
+const float roverTexels[61716];
+const float roverNormals[92574];
+
+const int roverMaterials;
+const int roverFirsts[10];
+const int roverCounts[10];
+
+const float roverDiffuses[10][3];
+const float roverSpeculars[10][3];
+
